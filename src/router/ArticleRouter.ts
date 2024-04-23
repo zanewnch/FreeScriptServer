8610125c32e63c38aa139bae5b54f163eceee7d1
zanewnch <zanewnch@gmail.com>
2024-04-23 21:51:58 +0800

    deprecate the autoswagger but use postman api to generate the api document.

--- src/router/ArticleRouter.ts
+++ src/router/ArticleRouter.ts
@@ -10,143 +10,49 @@ export default (router: express.Router) => {
   const checkAuthCookie = new CheckAuthCookie();
   const articleRepo = new ArticleRepo();
 
-  /*
-    swagger autogen 有一個bug, 如果沒有在router裡面直接寫function, 而是直接 controller, 不管在router function or controller function add swagger comment 他都會找不到.
-   
-    唯一的solution就是在router 寫function, 然後function 裡面再call controller and middleware(記得要傳req,res,next進去)
-   */
-
   router.get("/api/setting", (req, res) => {
     res.render("SettingView", {
       username: "zanewnch",
     });
   });
 
-  router.get("/api/article", checkAuthCookie.checkAuthCookie, (req, res) => {
-    /* 
-    #swagger.tags = ['Article']
-    #swagger.summary = 'get article by pagination'
-    #swagger.parameters['pageNum'] = {
-      in: 'query',
-      description: 'page number',
-      required: true,
-      type: 'integer'
-    }
-    #swagger.parameters['pageSize'] = {
-      in: 'query',
-      description: 'page size',
-      required: true,
-      type: 'integer'
-    }
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema: {
-        $ref: "#/definitions/ArticleGet200"
-      }
-    }
-    #swagger.responses[500] = {
-      description: 'Error',
-      schema: {
-        $ref: "#/definitions/error"
-      }
-    }
-    
-     */
-    articleController.get(req, res);
-  });
+  router.get(
+    "/api/article",
+    checkAuthCookie.checkAuthCookie,
+    articleController.get
+  );
 
   router.get(
     "/api/article/:author/:title",
     checkAuthCookie.checkAuthCookie,
-    (req, res) => {
-      /* 
-    #swagger.tags = ['Article']
-    #swagger.summary = 'Get specified article'
-    #swagger.parameters['author'] = {
-      in: 'path',
-      description: 'author name',
-      required: true,
-      type: 'string'
-    }
-    #swagger.parameters['title'] = {
-      in: 'path',
-      description: 'article title',
-      required: true,
-      type: 'string'
-    }
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema: {
-        $ref: "#/definitions/ArticleGet200"
-      }
-    }
-    #swagger.responses[500] = {
-      description: 'Error',
-      schema: {
-        $ref: "#/definitions/error"
-      }
-    }
-    */
-      articleController.getSpecifiedArticle(req, res);
-    }
+    articleController.getSpecifiedArticle
   );
 
   // get data
-  router.get("/api/allArticle", checkAuthCookie.checkAuthCookie, (req, res) => {
-    /* 
-    #swagger.tags = ['Article']
-    #swagger.summary = 'Get all articles'
-    #swagger.description = 'Get all articles description'
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema: {
-        $ref: "#/definitions/ArticleGet200"
-      }
-    }
-    */
-    articleController.getAll(req, res);
-  });
+  router.get(
+    "/api/allArticle",
+    checkAuthCookie.checkAuthCookie,
+    articleController.getAll
+  );
 
-  router.get("/api/search", checkAuthCookie.checkAuthCookie, (req, res) => {
-    articleController.search(req, res);
-  });
+  router.get(
+    "/api/search",
+    checkAuthCookie.checkAuthCookie,
+    articleController.search
+  );
 
-  router.delete("/api/article", checkAuthCookie.checkAuthCookie, (req, res) => {
-    articleController.deleteWithoutContent(req, res);
-  });
+  router.delete(
+    "/api/article",
+    checkAuthCookie.checkAuthCookie,
+    articleController.deleteWithoutContent
+  );
 
   // create data
-  router.post("/api/article", checkAuthCookie.checkAuthCookie, (req, res) => {
-    /* 
-    #swagger.tags = ['Article']
-    #swagger.summary = 'Create an article'
-    #swagger.description = 'Create an article description'
-    #swagger.parameters['data'] = {
-      in: 'body',
-      description: 'Article interface',
-      required: true,
-      type: 'object',
-      schema: {
-        $ref: "#/definitions/ArticleCreateParam"
-      }
-    }
-
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema:{
-        $ref: "#/definitions/ArticleCreate200"
-      }
-    }
-
-    #swagger.responses[500] = {
-      description: 'Error',
-      schema: {
-        $ref: "#/definitions/error"
-      }
-    }
-    */
-    articleController.create(req, res);
-  });
+  router.post(
+    "/api/article",
+    checkAuthCookie.checkAuthCookie,
+    articleController.create
+  );
 
   router.get(
     "/api/article/specific-tags-articles",
@@ -156,7 +62,7 @@ export default (router: express.Router) => {
       let result: object[] = await articleRepo.getSpecificTagsArticles(
         tag["tags"]
       );
-      
+
       res.status(200).json(Result.successWithData(result));
     }
   );
@@ -180,105 +86,31 @@ export default (router: express.Router) => {
   );
 
   router.get("/api/article/staff-picks", async (req, res) => {
-    /* 
-
-    #swagger.tags = ['Article']
-    #swagger.summary = 'Get staff picks for 3 articles'
-    #swagger.description = 'Get staff picks description'
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema: {
-        $ref: "#/definitions/staffPicksGet200"
-      }
-    }
-
-    #swagger.responses[500] = {
-      description: 'Error',
-      schema: {
-        $ref: "#/definitions/error"
-      }
-    }
-
-    */
     const result: object[] = await articleRepo.getStaffPicks();
-    
+
     res.status(200).json(Result.successWithData(result));
   });
 
   // test for knowing frontend send data
   router.get("/api/test", checkAuthCookie.checkAuthCookie, (req, res) => {
-    /* 
-      這邊的syntax 是使用 swagger 2.0
-
-       #swagger.tags = ['Article']
-
-       #swagger.summary = 'Get all articles'
-
-       #swagger.description = 'Get all articles description'
-
-        #swagger.parameters['pageNum'] = {
-          in: 'query',
-          description: 'page number',
-          required: true,
-          type: 'integer'
-        }
-
-        #swagger.parameters['pageSize'] = {
-          in: 'query',
-          description: 'page size',
-          required: true,
-          type: 'integer'
-        }
-
-        #swagger.responses[200] = {
-          description: 'Success',
-          schema: {
-            code:1,
-            msg:"null",
-            data:"null",
-          }
-        }
-
-        #swagger.responses[500] = {
-          description: 'Error',
-          schema: {
-            test:"test"
-          }
-        }
-
-        #swagger.operationId = 'getAllArticle'
-
-        #swagger.security = [{
-          "apiKeyAuth": []
-        }]
-
-        #swagger.securityDefinitions = {
-          apiKeyAuth: {
-            type: 'apiKey',
-            in: 'header',
-            name: 'x-auth-token'
-          }
-        }
-     */
-
     // which mean request with query string
     if (Object.keys(req.query).length !== 0) {
       const result: object = req.query;
-      
+
       res.status(200).json(Result.successWithData(result));
     }
 
     // which mean request with json content
     if (Object.keys(req.body).length !== 0) {
       const result: object = req.body;
-      
+
       res.status(200).json(Result.successWithData(result));
     }
 
     // which mean request with path variable
     if (Object.keys(req.params).length !== 0) {
       const result: object = req.params;
-      
+
       res.status(200).json(Result.successWithData(result));
     }
   });
@@ -286,14 +118,14 @@ export default (router: express.Router) => {
     // which mean request with query string
     if (Object.keys(req.query).length !== 0) {
       const result: object = req.query;
-      
+
       res.status(200).json(Result.successWithData(result));
     }
 
     // which mean request with json content
     if (Object.keys(req.body).length !== 0) {
       const result: object = req.body;
-      
+
       res.status(200).json(Result.successWithData(result));
     }
 
--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -4,7 +4,7 @@ import { CheckAuthCookie } from "../middleware/CheckAuthCookie";
 import { UserRepo } from "../model/UserRepo";
 import { UserController } from "../controller/UserController";
 import { Result } from "../utils/Result";
-import { JWT } from '../utils/JWT';
+import { JWT } from "../utils/JWT";
 
 // @ts-ignore
 export default (router: express.Router) => {
@@ -23,133 +23,80 @@ export default (router: express.Router) => {
   router.post(
     "/api/user/local-signIn",
     checkAuthCookie.checkAuthCookie,
-    (req: express.Request, res: express.Response) => {
-      /*
 
-    #swagger.tags = ['User']
-    #swagger.summary = 'User local login'
-
-    #swagger.parameters['data'] = {
-      in: 'body',
-      description: 'user data',
-      required: true,
-      type: 'object',
-      schema: { $ref: "#/definitions/UserLocalSignIn" }
-    }
-
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema: { $ref: "#/definitions/UserLocalSignIn" }
-    }
-
-    #swagger.responses[400] = {
-      description: 'Error',
-      schema: { $ref: "#/definitions/error" }
-    }
-    */
-
-      userController.localSignIn(req, res);
-    }
+    userController.localSignIn
   );
 
   router.post(
     "/api/user/google-signIn",
     checkAuthCookie.checkAuthCookie,
-    (req: express.Request, res: express.Response) => {
-      /* 
-    #swagger.tags = ['User']
-    #swagger.summary = 'User google login'
 
-    #swagger.parameters['data'] = {
-      in: 'body',
-      description: 'user data',
-      required: true,
-      type: 'object',
-      schema: { $ref: "#/definitions/UserGoogleSignIn" }
-    }
-
-    #swagger.responses[200] = {
-      description: 'Success',
-      schema: { $ref: "#/definitions/success" }
-    }
-
-    #swagger.responses[400] = {
-      description: 'Error',
-      schema: { $ref: "#/definitions/error" }
-    }
-    */
-
-      userController.googleSignIn(req, res);
-    }
+    userController.googleSignIn
   );
 
   router.post(
     "/api/user/local-jwt",
-    (req: express.Request, res: express.Response) => {
-      userController.generateLocalJwtToken(req, res);
-    }
+
+    userController.generateLocalJwtToken
   );
 
-  router.put("/api/user", checkAuthCookie.checkAuthCookie, (req, res) => {
-    /* 
-    
-    #swagger.tags = ['User']
-    #swagger.summary = 'Update user'
+  router.put(
+    "/api/user",
+    checkAuthCookie.checkAuthCookie,
+    userController.updateUser
+  );
 
-
-    */
-    userController.updateUser(req, res);
-  });
-
-  router.post('/api/user/set-cookie', (req, res) => {
+  router.post("/api/user/set-cookie", (req, res) => {
     let user = req.body;
-    if(user['username']){
-      let token = JWT.createToken(user['username']);
+    if (user["username"]) {
+      let token = JWT.createToken(user["username"]);
 
       // 然而，由於你已經將 httpOnly 選項設置為 true，這個 cookie 不能被 JavaScript 存取，這是一種安全措施，用來防止跨站腳本攻擊（XSS）。
-      res.cookie('login-token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
-      }).status(200).send(Result.successWithData(token));
-      
-    }else if(user['displayName']){
-      let token = JWT.createToken(user['displayName']);
+      res
+        .cookie("login-token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .status(200)
+        .send(Result.successWithData(token));
+    } else if (user["displayName"]) {
+      let token = JWT.createToken(user["displayName"]);
 
-      res.cookie('login-token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
-      }).status(200).send(Result.successWithData(token));
+      res
+        .cookie("login-token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .status(200)
+        .send(Result.successWithData(token));
     }
-    
-    
-
-    
   });
-  router.get('/api/user/verify-login', (req, res) => {
-    let token = req.cookies['login-token'];
+  router.get("/api/user/verify-login", (req, res) => {
+    let token = req.cookies["login-token"];
     console.log(token);
-    if(token){
-      let result = JWT.verifyToken(token)
-      if(result){
+    if (token) {
+      let result = JWT.verifyToken(token);
+      if (result) {
         res.status(200).send(Result.successWithData(result));
-    }else{
-      res.status(200).send(Result.error('No token'));
+      } else {
+        res.status(200).send(Result.error("No token"));
+      }
     }
-}});
+  });
 
-router.get('/api/user/decode-login', (req, res) => {
-  let token = req.cookies['login-token'];
-  console.log(token);
-  if(token){
-    let result = JWT.decodeToken(token);
-    console.log(result);
-    if(result){
-      res.status(200).send(Result.successWithData(result));
-  }else{
-    res.status(200).send(Result.error('No token'));
-  }
-}});
-  
+  router.get("/api/user/decode-login", (req, res) => {
+    let token = req.cookies["login-token"];
+    console.log(token);
+    if (token) {
+      let result = JWT.decodeToken(token);
+      console.log(result);
+      if (result) {
+        res.status(200).send(Result.successWithData(result));
+      } else {
+        res.status(200).send(Result.error("No token"));
+      }
+    }
+  });
 };
