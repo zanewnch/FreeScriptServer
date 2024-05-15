9d1f4a9b6d4abc4c8533013c8aa2e63c198e0a57
Zane <zanewnch@gmail.com>
2024-05-16 07:13:57 +0800

    temp.

---
+++ src/public/syntaxPractice.ts
@@ -0,0 +1,16 @@
+interface Test {
+    name: string;
+    age: number;
+}
+
+const test:Test = {
+    name: 'test',
+    age: 20
+}
+
+class TestClass{
+    public name:string;
+    public age:number;
+}
+
+const testClass:TestClass = new TestClass();
